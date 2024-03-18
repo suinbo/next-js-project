68a0d93b7e30335cdab320fb6ac5004dd113540b
suinbo <suin9610@gmail.com>
2024-03-18 09:57:16 +0900

    Feat: menu1 페이지 에러, 로딩 핸들링
    - react-error-boundary 추가
    - Suspense fallback 추가

--- my-app/src/app/menu1/layout.tsx
+++ my-app/src/app/menu1/layout.tsx
@@ -1,7 +1,10 @@
 /** 이벤트 핸들러를 클라이언트 컴포넌트의 props로 전달 X
  * => 서버 컴포넌트에서 클라이언트 컴포넌트로 전환  */
 "use client"
-import { useRef } from "react"
+import { Suspense, useRef } from "react"
+import Loading from "./loading"
+import { ErrorBoundary } from "react-error-boundary"
+import Error from "./error"
 
 export default function RootLayout({
     children,
@@ -21,34 +24,41 @@ export default function RootLayout({
     }
 
     const Menus = () =>
-        [...Array(40)].map((_, index) => (
-            <li key={index} className="flex flex-col font-semibold" onClick={() => onClickLi(index)}>
-                {`Menu ${index + 1}`}
-                <span
-                    className="font-normal text-sm"
-                    onClick={e => e.stopPropagation()} //이벤트 버블링 방지 (상위로부터)
-                    ref={element => {
-                        spanRefs.current[index] = element
-                    }}>{`${index + 1}-1`}</span>
-            </li>
-        ))
+        <ul className="h-[95%] flex flex-col gap-5 overflow-scroll overflow-x-hidden">
+            {[...Array(40)].map((_, index) => (
+                <li key={index} className="flex flex-col font-semibold cursor-pointer" onClick={() => onClickLi(index)}>
+                    {`Menu ${index + 1}`}
+                    <span
+                        className="font-normal text-sm"
+                        onClick={e => e.stopPropagation()} //이벤트 버블링 방지 (상위로부터)
+                        ref={element => {
+                            spanRefs.current[index] = element
+                        }}>{`${index + 1}-1`}
+                    </span>
+                </li>
+            ))}
+        </ul>
 
     return (
-        <div>
-            {/** Header */}
-            <header className="p-2 flex items-center justify-center bg-slate-200">
-                <h1 className="font-roboto text-2xl font-bold underline">Header</h1>
-            </header>
+        <ErrorBoundary fallback={<Error/>}>
+            <div> 
+                {/** Header */}
+                <header className="p-2 flex items-center justify-center bg-slate-200">
+                    <h1 className="font-roboto text-2xl font-bold underline">Header</h1>
+                </header>
 
-            {/** Navagation */}
-            <nav className="p-8 absolute h-[95%] w-1/5 bg-slate-100">
-                <ul className="h-full flex flex-col gap-5 overflow-scroll overflow-x-hidden">
+                {/** Navagation */}
+                <nav className="p-8 absolute h-[100vh] w-1/5 bg-slate-100">
                     <Menus />
-                </ul>
-            </nav>
+                </nav>
 
-            {/** Content */}
-            <div className="absolute h-[95%] w-4/5 left-[calc(1/5*100%)]">{children}</div>
-        </div>
+                {/** Content */}
+                <Suspense fallback={<Loading/>}>
+                    <div className="absolute h-[calc(100vh-48px)] w-4/5 left-[calc(1/5*100%)]">
+                        {children}
+                    </div>
+                </Suspense>
+            </div>
+        </ErrorBoundary>
     )
 }
--- my-app/src/app/menu1/page.tsx
+++ my-app/src/app/menu1/page.tsx
@@ -3,19 +3,20 @@ export default function Page() {
         [...Array(40)].map((_, index) => (
             <li
                 key={`content_${index}`}
-                className="w-[calc((100%-48px)/2)] lg:w-[calc((100%-70px)/3)] h-40 bg-slate-100 p-4 cursor-pointer rounded-2xl hover:border hover:transition hover:duration-500 hover:border-indigo-600">
+                className="w-[calc((100%-48px)/2)] lg:w-[calc((100%-70px)/3)] h-40 bg-slate-100 p-4 cursor-pointer rounded-2xl hover:border hover:transition hover:duration-500 hover:border-slate-600">
                 <span className="text-sm">NO {index + 1}.</span>
             </li>
         ))
 
     return (
-        <div className="p-8 flex flex-col gap-10 h-full">
-            <p className="text-xl font-semibold">Title</p>
-            <div className="overflow-scroll overflow-x-hidden">
-                <ul className="flex w-full flex-wrap gap-6">
+        <div className="p-8 flex flex-col gap-10">
+            <h1 className="font-bold text-2xl">Content</h1>
+            <div className="m-5 h-[820px]">
+                <p className="mb-8 font-bold text-lg">Title</p>
+                <ul className="flex w-full h-[100%] flex-wrap gap-6 overflow-scroll overflow-x-hidden">
                     <Li />
                 </ul>
             </div>
         </div>
-    )
+            )
 }
