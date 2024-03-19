4fa74883185a7277fee43eb30ea1bd2608658db8
suinbo <suin9610@gmail.com>
2024-03-19 10:16:12 +0900

    Chore: 코드 정리

--- my-app/src/app/dashboard/index.tsx
+++ my-app/src/app/dashboard/index.tsx
@@ -3,15 +3,9 @@ import Link from "next/link"
 export default function Dashboard({ children }: { children?: React.ReactNode }) {
     return (
         <section>
-            <nav>
-                <Link href="/">Home</Link>
-            </nav>
-            <nav>
-                <Link href="/menu1">Menu 1</Link>
-            </nav>
-            <nav>
-                <Link href="/menu2">Menu 2</Link>
-            </nav>
+            <Link href="/">Home</Link>
+            <Link href="/menu1">Menu 1</Link>
+            <Link href="/menu2">Menu 2</Link>
             {children}
         </section>
     )
