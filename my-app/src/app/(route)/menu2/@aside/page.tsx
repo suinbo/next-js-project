98b67533a0314e62360340a527f7b17044f58430
suinbo <suin9610@gmail.com>
2024-03-26 13:18:11 +0900

    Feat: 위젯 컴포넌트 추가

--- my-app/src/app/(route)/menu2/@aside/page.tsx
+++ my-app/src/app/(route)/menu2/@aside/page.tsx
@@ -1,72 +1,83 @@
+"use client"
+
+import Widget from "@/app/_component/Widget"
 import { transactions } from "@/app/_mocks/constant"
 import { addCommaOnNum } from "@/app/_utils/constant"
 import { chatbot, person } from "@/asset/images"
 import Image from "next/image"
+import { useRef, useState } from "react"
 
 export default function Aside() {
+    const divRefs = useRef<HTMLDivElement>(null)
+    const [openWidget, setOpenWidget] = useState<boolean>(false)
+
     const TransactionList = () => {
         const iconElement: { [key: string]: React.ReactElement } = {
-            plus: <i className="xi-plus-circle xi-2x text-red-500"></i>,
-            minus: <i className="xi-minus-circle xi-2x text-blue-500"></i>,
+            plus: <i className="xi-plus-circle xi-x text-red-500"></i>,
+            minus: <i className="xi-minus-circle xi-x text-blue-500"></i>,
         }
 
-        return transactions.map(({ place, type, won }, index) => (
-            <li key={index} className="mb-7 mr-8 flex items-center justify-between">
-                <span className="flex gap-8">
-                    {iconElement[type]}
-                    <a href="">{place}</a>
-                </span>
-                <span>
-                    <a>₩ {addCommaOnNum(won)}</a>
-                </span>
-            </li>
+        return transactions.map(({ place, type, won, description }, index) => (
+            <div key={index} className="mb-8 mr-8 flex items-center justify-between">
+                <div className="flex items-center gap-7">
+                    <span>{iconElement[type]}</span>
+                    <div className="flex flex-col">
+                        <span>{place}</span>
+                        <span className="text-xs text-slate-500">{description}</span>
+                    </div>
+                </div>
+                <div>
+                    <span className="text-slate-200">₩ {addCommaOnNum(won)}</span>
+                </div>
+            </div>
         ))
     }
 
     return (
-        <div className="m-4 flex flex-col gap-4 text-slate-300">
-            <div className="m-3 flex items-center justify-between">
-                <div className="flex cursor-pointer">
-                    <i className="xi-bell xi-x"></i>
-                    <span className="right-0 h-[4px] w-[4px] rounded-xl bg-red-500"></span>
-                </div>
-                <div className="flex items-center gap-6">
-                    <span>Suin Hwangbo</span>
-                    <span className="h-[40px] w-[40px] rounded-xl bg-[#5a8df9]">
-                        <Image
-                            src={person}
-                            alt="프로필이미지"
-                            className="relative left-1/2 top-1/2 translate-x-[-50%] translate-y-[-50%] transform"
-                        />
-                    </span>
-                </div>
-            </div>
-            <div className="m-3 rounded-3xl bg-gradient-to-br from-[#fba4ac] to-[#8099e6] p-8 text-blue-50">
-                <div className="flex flex-col gap-8">
-                    <div>ID 123.456.789</div>
-                    <div className="flex flex-col gap-2">
-                        <span>Your Money</span>
-                        <span className="text-3xl font-bold text-white">₩ 50,000</span>
+        <>
+            <div className="m-4 flex flex-col gap-4 text-slate-300">
+                <div className="m-3 flex items-center justify-between" ref={divRefs}>
+                    <div className="ml-1.5 flex cursor-pointer" onClick={() => setOpenWidget(!openWidget)}>
+                        <i className="xi-bell xi-x"></i>
+                        <span className="right-0 h-[4px] w-[4px] rounded-xl bg-red-500"></span>
+                    </div>
+                    <div className="flex items-center gap-6">
+                        <span>Suin Hwangbo</span>
+                        <span className="h-[40px] w-[40px] rounded-xl bg-[#5a8df9]">
+                            <Image
+                                src={person}
+                                alt="프로필이미지"
+                                className="relative left-1/2 top-1/2 translate-x-[-50%] translate-y-[-50%] transform"
+                            />
+                        </span>
                     </div>
                 </div>
-            </div>
-            <div className="m-3">
-                <div className="flex flex-col gap-8">
-                    <p className="text-xl">Last Transaction</p>
-                    <div>
-                        <ul className="h-[420px] overflow-scroll overflow-x-hidden">
+                <div className="m-3 rounded-3xl bg-gradient-to-br from-[#fba4ac] to-[#8099e6] p-8 text-blue-50">
+                    <div className="flex flex-col gap-8">
+                        <div>ID 123.456.789</div>
+                        <div className="flex flex-col gap-2">
+                            <span>Your Money</span>
+                            <span className="text-3xl font-bold text-white">₩ 50,000</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="m-3">
+                    <div className="flex flex-col gap-8">
+                        <p className="text-lg font-semibold">Last Transaction</p>
+                        <div className="h-[420px] overflow-scroll overflow-x-hidden">
                             <TransactionList />
-                        </ul>
+                        </div>
                     </div>
                 </div>
+                <div className="relative bottom-[15px] left-[326px] h-[60px] w-[60px] cursor-pointer rounded-full shadow-[0_0_8px_0_rgba(0,0,0,0.1)] shadow-slate-500/40">
+                    <Image
+                        src={chatbot}
+                        alt="챗봇"
+                        className="relative left-1/2 top-1/2 translate-x-[-50%] translate-y-[-50%] transform"
+                    />
+                </div>
             </div>
-            <div className="bottom-[14px] left-[326px] h-[60px] w-[60px] cursor-pointer rounded-full shadow-[0_0_8px_0_rgba(0,0,0,0.1)] shadow-slate-500/40">
-                <Image
-                    src={chatbot}
-                    alt="챗봇"
-                    className="relative left-1/2 top-1/2 translate-x-[-50%] translate-y-[-50%] transform"
-                />
-            </div>
-        </div>
+            {openWidget && <Widget type="info" />}
+        </>
     )
 }
---
+++ my-app/src/app/_component/Widget.tsx
@@ -0,0 +1,38 @@
+type WidgetType = "info" | "chat"
+
+export default function Widget({ type }: { type: WidgetType }) {
+    const widgetPos: { [key in WidgetType]: string } = {
+        info: `w-[calc(23%-160px)] top-[86px] right-[144px]`,
+        chat: `w-[386px] top-[40px]`,
+    }
+
+    const AlarmList = () =>
+        [...Array(40)].map((_, index) => (
+            <li key={index} className="mb-3 flex flex-col rounded-lg border border-[#363c69] p-5">
+                Description ...
+                <span className="mt-3 text-xs text-[#536da5]">2024-03-26 18:00:00</span>
+            </li>
+        ))
+
+    return (
+        <div
+            className={`
+                absolute p-6 ${widgetPos[type]} h-[610px] rounded-2xl bg-[#13162b]  
+                before:absolute before:left-4 before:top-[-26px] before:h-0 before:w-0 
+                before:border-b-[16px] before:border-l-[14px] before:border-r-[14px] before:border-t-[12px]
+                before:border-b-slate-900 before:border-l-transparent before:border-r-transparent before:border-t-transparent 
+                before:bg-transparent before:content-[""]`}>
+            <div className="h-[100%]">
+                <div className="mb-8 flex items-center justify-between text-slate-400">
+                    <span className="text-sm font-bold">Notice</span>
+                    <i className="xi-plus-min xi-x cursor-pointer font-light"></i>
+                </div>
+                <div className="h-[calc(100%-60px)]">
+                    <ul className="h-full overflow-scroll overflow-x-hidden pr-3 text-sm text-slate-300">
+                        <AlarmList />
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
