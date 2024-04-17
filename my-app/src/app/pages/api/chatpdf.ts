925675da50dfc45f8db889f285346115065a422b
suinbo <suin9610@gmail.com>
2024-04-17 13:51:04 +0900

    Feat: chatPDF API 연동

---
+++ my-app/src/app/pages/api/chatpdf.ts
@@ -0,0 +1,47 @@
+import { instance, instanceConfig } from "@/app/_utils/apis/request"
+import { AxiosError, AxiosResponse } from "axios"
+
+const apiRequest = (req: instanceConfig) => {
+    return instance(req)
+        .then((response: AxiosResponse<any, any>) => {
+            const { data, headers } = response
+            return { data, headers }
+        })
+        .catch((error: AxiosError) => {
+            console.log(error)
+        })
+}
+
+export const chatPDFRequest = async (config: any) => {
+    const { /*key,*/ ...apiConfig } = config
+
+    const options = Object.assign(apiConfig, {
+        baseURL: "https://api.chatpdf.com",
+        method: "POST",
+        headers: {
+            "x-api-key": process.env.NEXT_PUBLIC_API_KEY,
+        },
+    })
+
+    const response = await apiRequest(options)
+    return response?.data ?? []
+}
+
+export async function getSourceId(req: FormData) {
+    return chatPDFRequest({ url: "/v1/sources/add-file", data: req })
+}
+
+export async function getChatAnswer(sourceId: string, content: string) {
+    return chatPDFRequest({
+        url: "/v1/chats/message",
+        data: {
+            sourceId,
+            messages: [
+                {
+                    role: "user",
+                    content,
+                },
+            ],
+        },
+    })
+}
